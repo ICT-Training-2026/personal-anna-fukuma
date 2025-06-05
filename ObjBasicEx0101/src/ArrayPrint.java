da1f427ba39288756b5be4f579c705eaae7006e7
250450913 <anna-fukuma@kccs.co.jp>
2025-06-05 11:27:02 +0900

    初回コミット

---
+++ ObjBasicEx0101/src/ArrayPrint.java
@@ -0,0 +1,11 @@
+public class ArrayPrint {
+
+	//配列要素の表示
+	public static void main(String[] args) {
+		int[] array = { 3, 8, 10, 2, 4, 5 };		// 配列初期化
+		
+		for (int i = 0; i < array.length; i++) {	// 全ての要素の表示
+			System.out.println(array[i]);
+		}
+	}
+}
